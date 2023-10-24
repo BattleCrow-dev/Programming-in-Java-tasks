223fe2ccafc5b2e5cf7ede2915f23ad638689833
BattleCrow-dev <pashkovvv00@gmail.com>
2023-10-24 03:18:56 +0300

    to the 16 practical task

--- src/ru/mirea/practice14/parse_money/Tester.java
+++ src/ru/mirea/practice14/parse_money/Tester.java
@@ -7,7 +7,7 @@ public class Tester {
     public static String line = "50.25 USD, 25 EU, 100 RUB, 55 ERR, 12.45 EU, 100 TYUE, w273yhogiweqourhqwruo, 0.004 EU.";
 
     public static void main(String[] args) {
-        Pattern pattern = Pattern.compile("((?<=\\s)|(?<=^))\\d+(\\.\\d{1,2})?\\s?(USD|EU|RUB)");
+        Pattern pattern = Pattern.compile("((?<!\\d)(?<!\\.))\\d+(\\.\\d{1,2})?\\s?(USD|EU|RUB)");
         Matcher matcher = pattern.matcher(line);
         while(matcher.find())
             System.out.print(matcher.group() + "; ");
